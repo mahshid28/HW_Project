2c4d6daba8f1a2b22daaa79435bede783eb7ac6e
mahshid <mahshidvafei08@gmail.com>
2023-06-13 18:23:37 +0330

    fix:  add apps name to setting and correct profile url

--- users/models.py
+++ users/models.py
@@ -1,15 +1,10 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext as _
 
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
-    REQUIRED_FIELDS = ['email']
-
-
 class Profile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(_("Bio"), max_length=300, blank=True)
     profile_picture = models.ImageField(_("Profile_pic"), upload_to='profile_pics', blank=True)
     phone_number = models.CharField(_("Phone_number"), max_length=20, blank=True)
--- users/views.py
+++ users/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def wellcome(request):
-    return render(request, 'user.html')
+    return render(request, 'users/user.html')
