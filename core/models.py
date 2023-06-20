50e7ad023acb026289a50b7118b276c86ee23fb8
mahshid <mahshidvafei08@gmail.com>
2023-06-20 23:24:40 +0330

    add my manager and soft delete models to core app

--- core/models.py
+++ core/models.py
@@ -1,9 +1,30 @@
 from django.db import models
+from uuid import uuid4
 from django.utils.translation import gettext as _
 
 class BaseModel(models.Model):
+    id = models.UUIDField(editable=False, primary_key=True, default=uuid4)
+
     class Meta:
         abstract = True
+
+class MyManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(is_deleted=False)
+
+
+class SoftDeleteModel(BaseModel):
+    objects = MyManager()
+
+    is_deleted = models.BooleanField(default=False, db_index=True)
+
+    def delete(self):
+        self.is_deleted = True
+        self.save()
+
+    class Meta:
+        abstract = True
+
 
 class TimeStampMixin:
     created_at = models.DateTimeField(_("Created time"), auto_now_add=True)
