37474099499a10e3ff349ef9403fa9d91eb7ad96
mahshid <mahshidvafei08@gmail.com>
2023-06-27 16:08:19 +0330

    costomize admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from posts.models import Post, Comment, Like
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('user', 'slug', 'updated_at')
+	search_fields = ('slug', 'content')
+	list_filter = ('updated_at',)
+	prepopulated_fields = {'slug':('content',)}
+	raw_id_fields = ('user',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ('user', 'related_post', 'created_at', 'is_reply')
+	raw_id_fields = ('user', 'related_post', 'reply_to')
+
+
+admin.site.register(Like)
--- posts/admin.py
+++ posts/admin.py
@@ -1,18 +1,2 @@
 from django.contrib import admin
-from .models import Post, Comment, Like, Tag, PostImage
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    #exclude = ("is_deleted",)
-    #fields = ("text", ("user", status), "tags")
-    fieldsets =(
-        ('details', {
-            'fields': ('text', ("user", status), "tags"),
-        }),
-    )
-
-admin.site.register(PostImage)
-admin.site.register(Like)
-admin.site.register(Comment)
-admin.site.register(Tag)
 
